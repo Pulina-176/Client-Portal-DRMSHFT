3e573e46b64d55efeaf7234480b63ba28c9b1694
Chamudi Malsha <161874193+ChamudiM@users.noreply.github.com>
2024-08-11 12:12:32 +0530

    create test api route

--- backend-portal/index.js
+++ backend-portal/index.js
@@ -1,9 +1,12 @@
 import express from "express";
-
+import clientRoutes from "./routes/client.route.js";
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ "clients": ["client1", "client2"] })
-})
+// app.get("/api", (req, res) => {
+//     res.json({ "clients": ["client1", "client2"] })
+// })
 
-app.listen(5000, () => console.log("Server running on port 5000"))
+app.listen(5000, () => console.log("Server running on port 5000"));
+
+app.use("/api/client", clientRoutes);
+
