d6aba4d0825724a94a78d86ba4f4764fa0788f10
Chamudi Malsha <161874193+ChamudiM@users.noreply.github.com>
2024-08-14 17:53:19 +0530

    bug fix

--- frontend-portal/src/pages/Home.jsx
+++ frontend-portal/src/pages/Home.jsx
@@ -16,20 +16,23 @@ const Home = () => {
 
   const inprogress_tasks = taskList.filter(task => task.status === "in progress") //getting the tasks that are currently in progress
 
-  let inprogress_task = inprogress_tasks[0]
+  let inprogress_task
 
-  console.log("import data working", import_data)
+  //console.log("import data working", import_data)
 
   if (inprogress_tasks.length === 0) {  // getting the first task expected to be complete in the inprogress_tasks array
-    inprogress_task = inprogress_tasks[0]  
+    inprogress_task = {
+      name: "No tasks in progress"}  
   }
   else {
     inprogress_task = inprogress_tasks[0]  // bubble sort method to get the task with the earliest due date
     for (let i=1; i<inprogress_tasks.length; i++){
       if(inprogress_tasks[i].dueDate < inprogress_task.dueDate){
         inprogress_task = inprogress_tasks[i]
+        console.log(inprogress_task)
     }
-  }  
+  } 
+} 
 
   return (
    <>
@@ -42,7 +45,7 @@ const Home = () => {
  
    </>
   )
-}
+
 
 }
 
