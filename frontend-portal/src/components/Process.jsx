b922871ec6170e11d239730657a7cb1945ec181f
ravindumunasinghe <ravindumunasinghecoc@gmail.com>
2024-11-07 21:05:30 +0530

    test 1

--- frontend-portal/src/components/Process.jsx
+++ frontend-portal/src/components/Process.jsx
@@ -83,10 +83,10 @@ const Process = ({ taskLIST }) => {
     <section class="relative flex flex-col justify-center overflow-hidden antialiased">
       <div class="w-full max-w-6xl mx-auto px-4 md:px-6 py-2">
         <div class="flex flex-col justify-center divide-y divide-slate-200 [&>*]:py-0 mb-6">
-          <div class="w-full max-w-3xl mx-auto">
-            <div class="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:ml-[8.75rem]  md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
+          <div class="w-full max-w-3xl mx-auto ">
+            <div class="  space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:ml-[8.75rem]  md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent">
               {taskLIST_sorted.map((task, index) => (
-                <div key={index} class="relative">
+                <div key={index} class="relative ">
 
                    
                   <div class="flex items-center justify-center ml-0 sm:ml-0 md:ml-[120px] lg:ml-[120px] w-10 h-10 rounded-full bg-white shadow md:order-1">
